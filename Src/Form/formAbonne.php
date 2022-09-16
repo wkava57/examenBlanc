985aa30db58746b2583c337deb5d88503574ad1b
kava <wabutrune.p@hotmail.com>
2022-09-16 14:36:16 +0200

    debut formAbonne

--- Src/Form/formAbonne.php
+++ Src/Form/formAbonne.php
@@ -3,20 +3,20 @@
 namespace Src\Form;
 
 use Src\Service\Form;
-use Src\Model\Abonne;
+use Src\Model\modelAbonne;
 
 
-class FormArticle
+class FormAbonne
 {
-    public static function buildAddArticle()
+    public static function buildAddAbonne()
     {
         $form = new Form();
 
-        $form->debutForm('post', URL_ROOT . 'admin/article/add')
-            ->ajoutLabelFor('text', 'Titre :')
-            ->ajoutInput('text', 'titre', ['titre' => 'article', 'class' => 'form-control'])
-            ->ajoutLabelFor('text', 'infoSport')
-            ->ajoutInput('text', 'infoSport', ['infoSport' => 'article', 'class' => 'form-control'])
+        $form->debutForm('post', URL_ROOT . 'admin/abonne/add')
+            ->ajoutLabelFor('number', 'id :')
+            ->ajoutInput('number', 'id', ['id' => 'abonne', 'class' => 'form-control'])
+            ->ajoutLabelFor('text', 'prenom')
+            ->ajoutInput('text', 'prenom', ['prenom' => 'abonne', 'class' => 'form-control'])
             ->ajoutLabelFor('text', 'journaliste')
             ->ajoutInput('text', 'journaliste', ['journaliste' => 'article', 'class' => 'form-control'])
             ->ajoutBouton('Ajouter un article', ['class' => 'btn btn-primary'])
